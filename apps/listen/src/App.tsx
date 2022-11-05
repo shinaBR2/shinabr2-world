4b23d20fca32dd8f048ba6d75822d53cd3ff6f93
shinaBR2 <shinaBR2@users.noreply.github.com>
2022-11-05 20:43:23 +0700

    Add firebase (#4)
    * First clone providers
    
    * Add firebase to providers
    
    * Remove providers
    
    * allowSyntheticDefaultImports
    
    * Add firebase
    
    * update dependencies
    
    * Update CI
    
    * Update live CI
    
    Co-authored-by: ShinaBR2 <shinabr2@DESKTOP-I9A1V7H.localdomain>

--- apps/listen/src/App.tsx
+++ apps/listen/src/App.tsx
@@ -1,19 +1,22 @@
 import * as React from "react";
 import "./App.css";
 import { CounterButton, NewTabLink } from "ui";
+import { AuthProvider } from "./providers/auth";
 
-function App() {
+const App = () => {
   return (
-    <div className="container">
-      <h1 className="title">Listen</h1>
-      <CounterButton />
-      <p className="description">
-        Built With{" "}
-        <NewTabLink href="https://turborepo.org/">Turborepo</NewTabLink> +{" "}
-        <NewTabLink href="https://vitejs.dev/">Vite</NewTabLink>
-      </p>
-    </div>
+    <AuthProvider>
+      <div className="container">
+        <h1 className="title">Listen</h1>
+        <CounterButton />
+        <p className="description">
+          Built With{" "}
+          <NewTabLink href="https://turborepo.org/">Turborepo</NewTabLink> +{" "}
+          <NewTabLink href="https://vitejs.dev/">Vite</NewTabLink>
+        </p>
+      </div>
+    </AuthProvider>
   );
-}
+};
 
 export default App;
