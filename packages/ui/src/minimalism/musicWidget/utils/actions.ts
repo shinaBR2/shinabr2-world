99f8ed926b57a481f5f24d3df1ac4a86d618e44a
ShinaBR2 <shinabr2@DESKTOP-I9A1V7H.localdomain>
2022-11-06 17:58:00 +0700

    MinimalismMusicWidget utils

---
+++ packages/ui/src/minimalism/musicWidget/utils/actions.ts
@@ -0,0 +1,40 @@
+const playAudio = (player: HTMLAudioElement) => {
+  if (player.paused) {
+    player.play();
+
+    return "play";
+  }
+
+  player.pause();
+  return "pause";
+};
+
+const muteAudio = (player: HTMLAudioElement) => {
+  if (player.muted) {
+    player.muted = false;
+    return "unmute";
+  }
+
+  player.muted = true;
+  return "mute";
+};
+
+const loopAudio = (player: HTMLAudioElement) => {
+  if (player.loop) {
+    player.loop = false;
+    return "unloop";
+  }
+
+  player.loop = true;
+  return "loop";
+};
+
+const seek = (player: HTMLAudioElement, position: number) => {
+  player.currentTime = position;
+
+  if (player.paused) {
+    player.play();
+  }
+};
+
+export { playAudio, muteAudio, loopAudio, seek };
