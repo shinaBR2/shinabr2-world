5e189ea69a4ab979bb048dbc268eb3c8fac4935e
ShinaBR2 <shinabr2@DESKTOP-I9A1V7H.localdomain>
2022-11-12 13:12:26 +0700

    Add visualizer

--- apps/listen/vite.config.ts
+++ apps/listen/vite.config.ts
@@ -1,8 +1,9 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { viteCommonjs, esbuildCommonjs } from "@originjs/vite-plugin-commonjs";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://github.com/vitejs/vite/issues/5308#issuecomment-1010652389
 export default defineConfig({
-  plugins: [viteCommonjs(), react()],
+  plugins: [viteCommonjs(), react(), visualizer()],
 });
