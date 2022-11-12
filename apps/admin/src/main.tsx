896550a9945d0a4fa14427ea4e601ebc28268505
ShinaBR2 <shinabr2@DESKTOP-I9A1V7H.localdomain>
2022-11-12 19:50:03 +0700

    UPdate main

--- apps/admin/src/main.tsx
+++ apps/admin/src/main.tsx
@@ -3,9 +3,12 @@ import * as ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
