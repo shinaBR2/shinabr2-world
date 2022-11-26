75200d9d6d2df67c7b558e8e6e9360f8848205bb
ShinaBR2 <shinabr2@DESKTOP-I9A1V7H.localdomain>
2022-11-26 14:36:35 +0700

    Done styling

--- packages/ui/src/listen/minimalism/music-widget/MusicWidget.tsx
+++ packages/ui/src/listen/minimalism/music-widget/MusicWidget.tsx
@@ -1,7 +1,4 @@
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import Controls from "./Controls";
 import Seeker from "./Seeker";
@@ -9,17 +6,16 @@ import Seeker from "./Seeker";
 import hooks from "core";
 //@ts-ignore
 import { SAudioPlayerAudioItem, SAudioPlayerLoopMode } from "core";
-import {
-  Box,
-  Grid,
-  ListItemText,
-  MenuItem,
-  MenuList,
-  Slide,
-} from "@mui/material";
+import { Box, Grid, Slide } from "@mui/material";
 import PlaylistButton from "./PlaylistButton";
 import { useRef, useState } from "react";
-import { PlayArrowRounded } from "@mui/icons-material";
+import {
+  StyledCard,
+  StyledCardActions,
+  StyledCardMedia,
+  StyledContent,
+} from "./Styled";
+import PlayingList from "./PlayingList";
 
 const { useSAudioPlayer } = hooks;
 
@@ -68,72 +64,68 @@ const MusicWidget = (props: MusicWidgetProps) => {
     if (!isPlay) {
       onPlay();
     }
+
+    setShowPlayinglist(false);
+  };
+  const isSelected = (id: string) => {
+    return audioList[index].id === id;
   };
 
-  console.log(audioList);
+  const controlProps = {
+    isPlay,
+    shuffle: isShuffled,
+    loopMode,
+    handlePlay: onPlay,
+    handlePrev: onPrev,
+    handleNext: onNext,
+    onShuffle,
+    onChangeLoopMode,
+  };
 
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardMedia component="img" alt={name} height="300" image={image} />
-      <CardContent ref={contentRef}>
-        <Box
-          component={Grid}
-          container
-          justifyContent="space-between"
-          alignItems="center"
-          mb={1}
-        >
-          <Typography gutterBottom variant="body2" component="p">
-            Now playing
+    <StyledCard>
+      <StyledCardMedia component="img" alt={name} image={image} />
+      <StyledContent ref={contentRef}>
+        <CardContent>
+          <Box
+            component={Grid}
+            container
+            justifyContent="space-between"
+            alignItems="center"
+            mb={1}
+          >
+            <Typography gutterBottom variant="body2" component="p">
+              {showPlayinglist ? "Playing list" : "Now playing"}
+            </Typography>
+            <PlaylistButton
+              onClick={() => setShowPlayinglist(!showPlayinglist)}
+            />
+          </Box>
+          <Typography gutterBottom variant="h4" component="strong">
+            {artistName}
           </Typography>
-          <PlaylistButton
-            onClick={() => setShowPlayinglist(!showPlayinglist)}
-          />
-        </Box>
-        <Typography gutterBottom variant="h4" component="strong">
-          {artistName}
-        </Typography>
-        <Typography gutterBottom variant="h5" component="p">
-          {name}
-        </Typography>
-        <Seeker {...getSeekerProps()} />
-      </CardContent>
-      <CardActions style={{ display: "block" }}>
-        <Controls
-          isPlay={isPlay}
-          handlePlay={onPlay}
-          handlePrev={onPrev}
-          handleNext={onNext}
-          shuffle={isShuffled}
-          onShuffle={onShuffle}
-          loopMode={loopMode}
-          onChangeLoopMode={onChangeLoopMode}
-        />
-      </CardActions>
-      <Slide direction="up" in={showPlayinglist} container={contentRef.current}>
-        <Box
-          position="absolute"
-          width="300px"
-          height="280px"
-          top="400px"
-          sx={{ backgroundColor: "red" }}
+          <Typography gutterBottom variant="h5" component="p">
+            {name}
+          </Typography>
+          <Seeker {...getSeekerProps()} />
+        </CardContent>
+        <StyledCardActions>
+          <Controls {...controlProps} />
+        </StyledCardActions>
+        <Slide
+          direction="up"
+          in={showPlayinglist}
+          container={contentRef.current}
         >
-          <MenuList>
-            {audioList.map((a) => {
-              return (
-                <MenuItem key={a.id} onClick={onSelect(a.id)}>
-                  <ListItemText>{a.name}</ListItemText>
-                  <Typography variant="body2" color="text.secondary">
-                    <PlayArrowRounded />;
-                  </Typography>
-                </MenuItem>
-              );
-            })}
-          </MenuList>
-        </Box>
-      </Slide>
-      <audio {...getAudioProps()} />
-    </Card>
+          <PlayingList
+            audioList={audioList}
+            onSelect={onSelect}
+            isSelected={isSelected}
+          />
+        </Slide>
+        <audio {...getAudioProps()} />
+      </StyledContent>
+    </StyledCard>
   );
 };
 
---
+++ packages/ui/src/listen/minimalism/music-widget/Styled.tsx
@@ -0,0 +1,63 @@
+import {
+  Box,
+  BoxProps,
+  Card,
+  CardActions,
+  CardActionsProps,
+  CardMedia,
+  CardMediaProps,
+  CardProps,
+} from "@mui/material";
+import { styled } from "@mui/material/styles";
+
+const cardWidth = 345;
+const imageHeight = 300;
+const contentHeight = 300;
+
+const StyledCard = styled(Card)<CardProps>(({ theme }) => {
+  return {
+    width: cardWidth,
+    [theme.breakpoints.down("sm")]: {
+      width: "100%",
+      maxWidth: cardWidth,
+    },
+  };
+}) as typeof Card;
+
+const StyledCardMedia = styled(CardMedia)<CardMediaProps>(({ theme }) => {
+  return {
+    height: imageHeight,
+  };
+}) as typeof CardMedia;
+
+const StyledContent = styled("div")<BoxProps>(() => {
+  return {
+    position: "relative",
+    height: contentHeight,
+  };
+}) as typeof Box;
+
+const StyledCardActions = styled(CardActions)<CardActionsProps>(() => {
+  return {
+    display: "block",
+  };
+}) as typeof CardActions;
+
+const StyledPlayingList = styled(Box)<BoxProps>(({ theme }) => {
+  return {
+    position: "absolute",
+    width: "100%",
+    height: "100%",
+    top: 56,
+    overflowY: "auto",
+    backgroundColor: theme.palette.common.white,
+  };
+}) as typeof Box;
+
+export {
+  StyledCard,
+  StyledCardMedia,
+  StyledContent,
+  StyledCardActions,
+  StyledPlayingList,
+};
