be44d1ed6118301edfd5108d3efe4646044f7494
ShinaBR2 <shinabr2@DESKTOP-I9A1V7H.localdomain>
2022-12-02 22:21:11 +0700

    Delete confirm

--- apps/admin/src/pages/listen/components/AudioCRUDFormDialog/AudioCRUDFormDialog.jsx
+++ apps/admin/src/pages/listen/components/AudioCRUDFormDialog/AudioCRUDFormDialog.jsx
@@ -5,10 +5,20 @@ import DialogTitle from "@mui/material/DialogTitle";
 import AudioCRUDForm from "./AudioCRUDForm";
 import IconButton from "@mui/material/IconButton";
 import CloseIcon from "@mui/icons-material/Close";
+import { Button, DialogActions, DialogContentText } from "@mui/material";
 
 const AudioCRUDFormDialog = (props) => {
   const { open, onClose, onConfirm, onDelete, isCreate, data } = props;
   const title = isCreate ? "Upload audio" : "Edit audio";
+  const [showDeleteConfirm, setShowDeleteConfirm] = React.useState(false);
+
+  const handleDelete = () => {
+    setShowDeleteConfirm(true);
+  };
+
+  const cancelDelete = () => {
+    setShowDeleteConfirm(false);
+  };
 
   return (
     <Dialog open={open} onClose={onClose}>
@@ -30,12 +40,27 @@ const AudioCRUDFormDialog = (props) => {
         ) : null}
       </DialogTitle>
       <DialogContent>
-        <AudioCRUDForm
-          data={isCreate ? null : data}
-          onConfirm={onConfirm(isCreate)}
-          onDelete={onDelete}
-        />
+        {showDeleteConfirm && (
+          <DialogContentText>Are you sure to delete?</DialogContentText>
+        )}
+        {!showDeleteConfirm && (
+          <AudioCRUDForm
+            data={isCreate ? null : data}
+            onConfirm={onConfirm(isCreate)}
+            onDelete={handleDelete}
+          />
+        )}
       </DialogContent>
+      {showDeleteConfirm && (
+        <DialogActions>
+          <Button onClick={cancelDelete} autoFocus>
+            Cancel
+          </Button>
+          <Button onClick={onDelete} color="error" variant="outlined">
+            Yes I understand the risk!
+          </Button>
+        </DialogActions>
+      )}
     </Dialog>
   );
 };
