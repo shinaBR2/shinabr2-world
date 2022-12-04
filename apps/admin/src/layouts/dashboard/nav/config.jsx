9cfdfc75629ec8450484f4980f28322a241ccd4e
ShinaBR2 <shinabr2@DESKTOP-I9A1V7H.localdomain>
2022-12-04 13:32:33 +0700

    Update nav

--- apps/admin/src/layouts/dashboard/nav/config.jsx
+++ apps/admin/src/layouts/dashboard/nav/config.jsx
@@ -51,16 +51,6 @@ const navConfig = [
     path: "/dashboard/blog",
     icon: icon("ic_blog"),
   },
-  {
-    title: "login",
-    path: "/login",
-    icon: icon("ic_lock"),
-  },
-  {
-    title: "Not found",
-    path: "/404",
-    icon: icon("ic_disabled"),
-  },
 ];
 
 export default navConfig;
