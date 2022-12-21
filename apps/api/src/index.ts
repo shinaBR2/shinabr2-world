5ba62383e5c64a5a27a84ad22fc4827d32091e33
ShinaBR2 <shinabr2@DESKTOP-I9A1V7H.localdomain>
2022-12-21 08:41:12 +0700

    First running Cloud Functions

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,10 +1,17 @@
-import { createServer } from "./server";
-import { log } from "logger";
+import * as functions from "firebase-functions";
+/* import { createServer } from "./server";
+import { log } from "logger"; */
 
 // eslint-disable-next-line turbo/no-undeclared-env-vars
-const port = process.env.PORT || 5001;
+/* const port = process.env.PORT || 5001;
 const server = createServer();
 
 server.listen(port, () => {
   log(`api running on ${port}`);
+}); */
+
+const hello = functions.https.onRequest((req, res) => {
+  res.send("Firebase Cloud Functions");
 });
+
+export { hello };
