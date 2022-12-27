e9fbd4757340dbffbb2db6751c51125c94c8bbe9
ShinaBR2 <ShinaBR2@gmail.com>
2022-12-27 20:16:37 +0700

    Add functionality

--- apps/api/src/admin/listen/homepage.ts
+++ apps/api/src/admin/listen/homepage.ts
@@ -1,7 +1,8 @@
+import { CallableContext } from "firebase-functions/v1/https";
 import { onCall, onRequest } from "../../singleton";
 import { dbBatchWrite, dbGetRef, dbRead } from "../../singleton/db";
 
-const saveFeelings = async (data: any) => {
+const saveFeelings = async (data: any, context: CallableContext) => {
   const { feelings } = data;
 
   console.log(JSON.stringify(feelings));
