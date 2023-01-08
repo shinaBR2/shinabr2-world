6d68aaf624d3a21a011ab204d7cd8824c6adf631
ShinaBR2 <ShinaBR2@gmail.com>
2023-01-08 11:13:08 +0700

    clean up

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -1,6 +1,3 @@
-export { NewTabLink } from "./NewTabLink"; // TODO remove this
-export { CounterButton } from "./CounterButton"; // TODO remove this
-
 export * as SUI from "./sui-base";
 export * as ListenUI from "./listen";
 export * as UniversalUI from "./universal";
