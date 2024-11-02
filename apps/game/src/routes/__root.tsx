013c6b13f62af9d97c3df6828dcf1b40dd3772ca
vuong <shinabr2@gmail.com>
2024-11-02 09:59:43 +0700

    Theme provider

--- apps/game/src/routes/__root.tsx
+++ apps/game/src/routes/__root.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import { UniversalUI } from "ui";
+
+const { UniversalMinimalismThemeProvider } = UniversalUI.Minimalism;
 
 export const Route = createRootRoute({
   component: () => (
-    <>
+    <UniversalMinimalismThemeProvider>
       <Outlet />
       <TanStackRouterDevtools />
-    </>
+    </UniversalMinimalismThemeProvider>
   ),
 });
