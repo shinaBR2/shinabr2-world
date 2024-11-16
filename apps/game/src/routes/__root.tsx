2e9dd2943491fa04f64dd1331553881d52d0c2c9
vuong <shinabr2@gmail.com>
2024-11-16 12:51:48 +0700

    Add react query

--- apps/game/src/routes/__root.tsx
+++ apps/game/src/routes/__root.tsx
@@ -1,15 +1,20 @@
-import React from "react";
-import { createRootRoute, Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
-import { UniversalUI } from "ui";
+import React from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+import { createRootRoute, Outlet } from '@tanstack/react-router';
+import { TanStackRouterDevtools } from '@tanstack/router-devtools';
+import { UniversalUI } from 'ui';
+
+const queryClient = new QueryClient();
 const { UniversalMinimalismThemeProvider } = UniversalUI.Minimalism;
 
 export const Route = createRootRoute({
   component: () => (
-    <UniversalMinimalismThemeProvider>
-      <Outlet />
-      <TanStackRouterDevtools />
-    </UniversalMinimalismThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <UniversalMinimalismThemeProvider>
+        <Outlet />
+        <TanStackRouterDevtools />
+      </UniversalMinimalismThemeProvider>
+    </QueryClientProvider>
   ),
 });
