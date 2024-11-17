569fc809f7ec951e66a8a554be3342899e5d7824
vuong <shinabr2@gmail.com>
2024-11-17 12:16:52 +0700

    Global Jest preset

---
+++ packages/jest-dom-preset/index.js
@@ -0,0 +1,20 @@
+// packages/config/jest-config/index.js
+/** @type {import('jest').Config} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.jsx?$': 'babel-jest',
+  },
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    // Add any module name mappings if needed
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  // setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  testPathIgnorePatterns: ['/node_modules/'],
+  verbose: true,
+};
