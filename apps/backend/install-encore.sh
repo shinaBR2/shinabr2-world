2d43d04429e9d96920605839141bc10cffedcaba
vuong <shinabr2@gmail.com>
2024-12-13 17:21:05 +0700

    Add auth

--- apps/backend/install-encore.sh
+++ apps/backend/install-encore.sh
@@ -3,4 +3,5 @@ if [ "$CI" = "true" ]; then
   curl -L https://encore.dev/install.sh | bash
   export ENCORE_INSTALL=/home/runner/.encore
   export PATH=/home/runner/.encore/bin:$PATH
+  encore auth login --auth-key=$ENCORE_AUTH_KEY
 fi
