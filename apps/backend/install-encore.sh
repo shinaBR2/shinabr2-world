94f87e26077b355a0032a44fa8a609c5d059c659
vuong <shinabr2@gmail.com>
2024-12-13 16:25:19 +0700

    store to github env

--- apps/backend/install-encore.sh
+++ apps/backend/install-encore.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ "$CI" = "true" ]; then
   curl -L https://encore.dev/install.sh | bash
-  export ENCORE_INSTALL="/home/runner/.encore"
-  export PATH="$ENCORE_INSTALL/bin:$PATH"
+  echo 'export ENCORE_INSTALL="/home/runner/.encore"' >> $GITHUB_ENV
+  echo 'export PATH="$ENCORE_INSTALL/bin:$PATH"' >> $GITHUB_ENV
 fi
