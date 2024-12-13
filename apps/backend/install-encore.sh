9a2dfc155a41263855a29d89d6ebbe61edfdfc88
vuong <shinabr2@gmail.com>
2024-12-13 17:08:56 +0700

    test

--- apps/backend/install-encore.sh
+++ apps/backend/install-encore.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 if [ "$CI" = "true" ]; then
   curl -L https://encore.dev/install.sh | bash
-  echo "export ENCORE_INSTALL=/home/runner/.encore" >> ~/.bashrc
-  echo "export PATH=/home/runner/.encore/bin:$PATH" >> ~/.bashrc
-  source ~/.bashrc
+  export ENCORE_INSTALL=/home/runner/.encore
+  export PATH=/home/runner/.encore/bin:$PATH
 fi
