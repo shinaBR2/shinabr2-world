d8781ca795ff67f159d49c1e4c42ea45bdbab020
vuong <shinabr2@gmail.com>
2024-12-14 15:51:35 +0700

    trigger deploy

--- apps/backend/health-check/index.ts
+++ apps/backend/health-check/index.ts
@@ -1,12 +1,5 @@
 import { api } from 'encore.dev/api';
 
-interface Response {
-  status: string;
-}
-
-export const get = api(
-  { expose: true, method: 'GET', path: '/' },
-  async (): Promise<Response> => {
-    return { status: 'ok' };
-  }
-);
+export const get = api({ expose: true, method: 'GET', path: '/' }, async () => {
+  return { status: 'ok' };
+});
