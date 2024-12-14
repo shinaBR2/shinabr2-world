dcad38cc9e615f4640ce682dbf65fcfc70455275
vuong <shinabr2@gmail.com>
2024-12-14 15:47:43 +0700

    remove

--- apps/backend/videos/encore.service.ts
+++ apps/backend/videos/encore.service.ts
@@ -1,8 +1,6 @@
 import { Service } from 'encore.dev/service';
 import { initializeApp } from 'firebase-admin/app';
 
-console.log(`apps backend`, process.env.DATABASE_URL);
-
 initializeApp({
   storageBucket: process.env.VITE_STORAGE_BUCKET,
 });
