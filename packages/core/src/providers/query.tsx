03d1e85789e2e0269c0c9da0e223882d1a7874f3
vuong <shinabr2@gmail.com>
2024-12-15 20:52:02 +0700

    query context

--- packages/core/src/providers/query.tsx
+++ packages/core/src/providers/query.tsx
@@ -1,12 +1,49 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { FC } from 'react';
+import { createContext, FC, useContext } from 'react';
 
 const queryClient = new QueryClient();
 
-const QueryProvider: FC<any> = ({ children }) => {
+interface QueryContextValue {
+  hasuraUrl: string;
+}
+
+interface Config {
+  hasuraUrl: string;
+}
+
+const QueryContext = createContext<QueryContextValue>({} as QueryContextValue);
+const QueryContextProvider = (props: { config: Config; children: any }) => {
+  const { config, children } = props;
+  const { hasuraUrl } = config;
+  const contextValue: QueryContextValue = {
+    hasuraUrl,
+  };
+
+  console.log(`hasura URL`, hasuraUrl);
+
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryContext.Provider value={contextValue}>
+      {children}
+    </QueryContext.Provider>
   );
 };
 
-export { QueryProvider };
+const QueryProvider: FC<any> = ({ config, children }) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <QueryContextProvider config={config}>{children}</QueryContextProvider>
+    </QueryClientProvider>
+  );
+};
+
+const useQueryContext = (): QueryContextValue => {
+  const context = useContext(QueryContext);
+
+  if (!context) {
+    throw new Error('useQueryContext must be used within an QueryProvider');
+  }
+
+  return context;
+};
+
+export { QueryProvider, useQueryContext };
