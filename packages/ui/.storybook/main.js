2f2a47885cac2bd933054c30ed091d18221e9388
vuong <shinabr2@gmail.com>
2024-12-18 19:40:42 +0700

    Move storybook to ui

---
+++ packages/ui/.storybook/main.js
@@ -0,0 +1,18 @@
+module.exports = {
+  stories: [
+    '../stories/**/*.stories.mdx',
+    '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+  ],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-vite',
+  },
+  features: {
+    storyStoreV7: true,
+  },
+};
--- packages/ui/src/listen/minimalism/music-widget/MusicWidget.stories.tsx
+++ packages/ui/src/listen/minimalism/music-widget/MusicWidget.stories.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import { ThemeProvider } from '@emotion/react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import { ListenUI } from 'ui';
+import hooks from 'core';
+import theme from '../theme';
 
-const { MusicWidget } = ListenUI.Minimalism;
+const { useSAudioPlayer } = hooks;
+
+import MusicWidget from './MusicWidget';
 
 const audioList = [
   {
+    id: 'uuid-1',
     src: 'https://res.cloudinary.com/shinabr2/video/upload/v1667828415/Public/Music/Japanese/Attack-on-Titan-Opening-1-Feuerroter-Pfeil-und-Bogen-Full-128-Linked-Horizon_1.mp3',
     name: 'Guren No Yumiya',
     artistName: 'Linked Horizon',
@@ -14,6 +18,7 @@ const audioList = [
       'https://res.cloudinary.com/shinabr2/image/upload/v1667828561/Public/Images/artworks-000141088556-xy2nav-t500x500.jpg',
   },
   {
+    id: 'uuid-2',
     src: 'https://res.cloudinary.com/shinabr2/video/upload/v1667831555/Public/Music/Japanese/Shinzou_wo_Sasageyo__-_Linked_Horizon.mp3',
     name: 'Shinzo wo Sasageyo',
     artistName: 'Linked Horizon',
@@ -22,17 +27,38 @@ const audioList = [
   },
 ];
 
+// export default {
+//   title: 'Music widget',
+//   component: MusicWidget,
+//   // argTypes: {
+//   //   audioList: any[],
+//   // },
+// } as ComponentMeta<typeof MusicWidget>;
+
 export default {
   title: 'Music widget',
   component: MusicWidget,
-  // argTypes: {
-  //   audioList: any[],
-  // },
+  decorators: [
+    Story => (
+      <ThemeProvider theme={theme}>
+        <Story />
+      </ThemeProvider>
+    ),
+  ],
 } as ComponentMeta<typeof MusicWidget>;
 
-const Template: ComponentStory<typeof MusicWidget> = args => (
-  <MusicWidget {...args} />
-);
+const Template: ComponentStory<typeof MusicWidget> = args => {
+  const hookResult = useSAudioPlayer({
+    audioList: args.audioList,
+    index: 0,
+  });
+  const props = {
+    ...args,
+    hookResult,
+  };
+
+  return <MusicWidget {...props} />;
+};
 
 export const Basic = Template.bind({});
 Basic.args = {
