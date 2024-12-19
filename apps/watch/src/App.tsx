098c29c8040a6ad5d4367da019a4f3e4ac1d1c52
vuong <shinabr2@gmail.com>
2024-12-19 19:03:03 +0700

    Add provider

--- apps/watch/src/App.tsx
+++ apps/watch/src/App.tsx
@@ -1,14 +1,32 @@
 import React from 'react';
+import { Auth, Query } from 'core';
 import { WatchUI, UniversalUI } from 'ui';
 
 const { Homepage } = WatchUI;
 const ThemeProvider = UniversalUI.Minimalism.UniversalMinimalismThemeProvider;
 
+const auth0Config = {
+  domain: import.meta.env.VITE_AUTH0_DOMAIN,
+  clientId: import.meta.env.VITE_AUTH0_CLIENT_ID,
+  audience: import.meta.env.VITE_HASURA_GRAPHQL_URL,
+  redirectUri: window.location.origin,
+};
+const queryConfig = {
+  hasuraUrl: import.meta.env.VITE_HASURA_GRAPHQL_URL,
+};
+
+console.log(`auth0Config`, auth0Config);
+console.log(`queryConfig`, queryConfig);
+
 const App = () => {
   return (
-    <ThemeProvider>
-      <Homepage />
-    </ThemeProvider>
+    <Auth.AuthProvider config={auth0Config}>
+      <Query.QueryProvider config={queryConfig}>
+        <ThemeProvider>
+          <Homepage />
+        </ThemeProvider>
+      </Query.QueryProvider>
+    </Auth.AuthProvider>
   );
 };
 
